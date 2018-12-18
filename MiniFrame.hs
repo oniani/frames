e8970fc3cd00261acfbfd2964c310cb000260aeb
David Oniani <onianidavid@gmail.com>
2018-12-18 02:22:38 -0600

    Added functions for dealing with data frames

--- MiniFrame.hs
+++ MiniFrame.hs
@@ -1,7 +1,7 @@
 {- |
 Module      :  MiniFrame.hs
 Description :  Module implements the MiniFrame and its core functionalities
-Copyright   :  (c) David Oniani
+Copyright   :  (c) David Oniani, 2018
 License     :  MIT
 
 Maintainer  :  onianidavid@gmail.com
@@ -10,21 +10,38 @@ Portability :  portable
 -}
 
 module MiniFrame
-    ( newMiniFrame
+    ( sampleMiniFrame
+
+    -- Retrieval
     , getName
     , getHeader
     , getFields
+    , getFieldByIndex
+    , getColumnFieldByIndex
+
+    -- Modification
+    , addField
+    , addColumn
+    , removeFieldByIndex
+    , removeColumnFieldByIndex
+
+    -- Pretty-printers
     , printName
     , printHeader
     , printFields
     , printMF
+
+    -- Lengths
+    , rowsNum
+    , columnsNum
     ) where
 
 import Data.List
 
 type Name   = String
 type Header = [String]
-type Fields = [[String]]
+type Field  = [String]
+type Fields = [Field]
 
 data MiniFrame = MiniFrame
     { name   ::  Name        -- Name of the MiniFrame
@@ -33,12 +50,12 @@ data MiniFrame = MiniFrame
     deriving (Eq)
 
 
--- | MiniFrame
-newMiniFrame :: MiniFrame
-newMiniFrame = MiniFrame name header fields
+-- | A sample MiniFrame
+sampleMiniFrame :: MiniFrame
+sampleMiniFrame = MiniFrame name header fields
     where
         name   = "Table name"
-        header = ["ColumnOne","ColumnTwo","ColumnThree","ColumnFour"]
+        header = ["First Column","Second Column","Third Column","Fourth Column"]
         fields = [["Row1-Col1","Row1-Col2","Row1-Col3","Row1-Col4"],["Row2-Col1","Row2-Col2","Row2-Col3","Row2-Col4"],
                   ["Row3-Col1","Row3-Col2","Row3-Col3","Row3-Col4"],["Row4-Col1","Row4-Col2","Row4-Col3","Row4-Col4"],
                   ["Row5-Col1","Row5-Col2","Row5-Col3","Row5-Col4"],["Row6-Col1","Row6-Col2","Row6-Col3","Row6-Col4"],
@@ -56,6 +73,22 @@ getHeader (MiniFrame name header fields) = header
 getFields :: MiniFrame -> [[String]]
 getFields (MiniFrame name header fields) = fields
 
+-- | Get the row at the particular index
+getFieldByIndex :: MiniFrame -> Integer -> Field
+getFieldByIndex (MiniFrame name header fields) index = fields !! fromInteger index
+
+-- | Get the column at the particular index
+getColumnFieldByIndex :: MiniFrame -> Integer -> Field
+getColumnFieldByIndex (MiniFrame name header fields) index = transpose fields !! fromInteger index
+
+-- | Add the field to the end of the data frame
+addField :: MiniFrame -> Field -> MiniFrame
+addField (MiniFrame name header fields) field = MiniFrame name header (fields ++ [field])
+
+-- | Remove the field at the particular index
+removeFieldByIndex :: MiniFrame -> Integer -> MiniFrame
+removeFieldByIndex (MiniFrame name header fields) index = MiniFrame (name header (take (index - 1) fields ++ drop index field))
+
 -- | Print the name of the data frame
 printName :: MiniFrame -> IO ()
 printName (MiniFrame name header fields) = print name
@@ -70,39 +103,38 @@ printFields (MiniFrame name header fields) = mapM_ print fields
 
 -- | Print the data frame
 printMF :: MiniFrame -> IO ()
-printMF (MiniFrame name header fields) = do
-                                            putStrLn (" " ++ replicate (length name) '_' ++ "\n|" ++ name ++ "|\n " ++ replicate (length name) '-' ++ "\n")
-                                            putStrLn (unwords header)
-                                            putStrLn (replicate (maximum (map (length . show) fields)) '=')
-                                            mapM_ putStrLn [intercalate " | " i | i <- fields]
-                                            --
-                                            -- MIGHT HAVE TO USE IT... THE TRANSPOSE FUNCTION SEEMS TO BE REALLY USEFUL
-                                            -- ONCE WE FIND THE MAXIMUM LENGTH OF THE STRING FOR EACH COLUMN, FOR EACH
-                                            -- STRING IN THE LIST, ADD (BIGGEST STRING SIZE - SIZE OF CURRENT STRING) NUMBER OF SPACES
-                                            --
-                                            -- putStrLn (" " ++ replicate (length name) '_' ++ "\n|" ++ name ++ "|\n " ++ replicate (length name) '-' ++ "\n")
-                                            -- mapM_ putStr (map (++"|") (init headerForPrettyPrint) ++ [last header ++ "\n"])
-                                            -- putStrLn (replicate (maximum (map (length . show) fields)) '=')
-                                            -- mapM_ print fields
-                                            -- where
-                                            --     -- +3 since the first header has to account for ( ["" ) - three characters in total
-                                            --     -- +2 since the rest of the headers have to account for just two characters - ( ," )
-                                            --     -- We subtract the length of (fst i) since we have to account for the length of the field entry
-                                            --     maxLengthPerColumn = transpose [map length i | i <- fields]
-                                            --     spacesPerColumn = (maximum (head maxLengthPerColumn) + 3) : [maximum i + 2 | i <- tail maxLengthPerColumn]
-                                            --     headerForPrettyPrint = [fst i ++ replicate (snd i - length (fst i)) ' ' | i <- zip header spacesPerColumn]
+printMF (MiniFrame name header fields) = do putStrLn (" " ++ replicate (length name) '_' ++ "\n|" ++ name ++ "|\n " ++ replicate (length name) '-' ++ "\n")
+                                            mapM_ putStr [fst i ++ replicate (snd i - length (fst i) + 3) ' ' | i <- zip header maxNumOfSpaces]
+                                            putStrLn ("\n" ++ replicate (maximum (map (length . show) fields)) '=')
+                                            mapM_ putStrLn [intercalate " | " i | i <- fieldsForPrettyPrint]
+                                            where
+                                                headerLengthList = map length header
+                                                maxLengthOfFieldsPerColumn = map maximum (transpose [map length i | i <- fields])
+                                                maxNumOfSpaces = [if uncurry (>) i then fst i else snd i | i <- zip headerLengthList maxLengthOfFieldsPerColumn]
+                                                fieldsForPrettyPrint = transpose [map (\n -> n ++ replicate (snd i - length n) ' ') (fst i) | i <- zip (transpose fields) maxNumOfSpaces]
+
+-- | Get the number of rows
+rowsNum :: MiniFrame -> Integer
+rowsNum (MiniFrame name header fields) = toInteger (length fields)
+
+-- | Get the number of columns
+columnsNum :: MiniFrame -> Integer
+columnsNum (MiniFrame name header fields) = toInteger (length (transpose fields))
+
 
 main = do
     -- putStrLn "Testing..."
     -- putStrLn "----------\n"
-    
-    let sampledf = newMiniFrame
 
-    -- print $ getName   sampledf
-    -- print $ getHeader sampledf
-    -- print $ getFields sampledf
+    let sampleMF = sampleMiniFrame
 
-    -- printName   sampledf
-    -- printHeader sampledf
-    -- printFields sampledf
-    printMF sampledf
+    -- print $ getName   sampleMF
+    -- print $ getHeader sampleMF
+    -- print $ getFields sampleMF
+
+    -- printName   sampleMF
+    -- printHeader sampleMF
+    -- printFields sampleMF
+    -- printMF sampleMF
+    printMF (addField sampleMF ["This", "is", "a", "test MiniFrame"])
+    printMF (removeFieldByIndex sampleMF 1)
